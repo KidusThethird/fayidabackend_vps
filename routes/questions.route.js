b4f96b95b5eda227b9a87190173e3dff21b3e276
kidus Tesfaye <kidus.z3rd@gmail.com>
2024-05-07 13:32:16 +0300

    update

--- routes/questions.route.js
+++ routes/questions.route.js
@@ -100,7 +100,6 @@ router.get("/accessquestions/:id", async (req, res, next) => {
 
     res.json(questionWithSignedUrls);
   } catch (error) {
-    console.log("Error from catch: " + error);
     next(error);
   }
 });
