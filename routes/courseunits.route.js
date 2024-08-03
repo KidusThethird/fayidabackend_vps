38ce1e16da7f6af4c972ac8d8c6038c5cdf8b9c2
kidus Tesfaye <kidus.z3rd@gmail.com>
2024-08-03 05:44:58 +0300

    update: course unit route edited

--- routes/courseunits.route.js
+++ routes/courseunits.route.js
@@ -63,13 +63,40 @@ router.get("/accessone/:studentId/:courseId/:unit", async (req, res, next) => {
     if (getCourseStudent) {
       const StudentCourseId = getCourseStudent.id;
 
-      await prisma.CourseUnits.create({
-        data: {
-          StudentCourseId: StudentCourseId,
+      const existingCourseUnit = await prisma.CourseUnits.findFirst({
+        where: {
+          StudentCourseId: getCourseStudent.id,
           unitNumber: req.params.unit,
-          status: true,
         },
       });
+
+      if (!existingCourseUnit) {
+        await prisma.CourseUnits.create({
+          data: {
+            StudentCourseId: getCourseStudent.id,
+            unitNumber: req.params.unit,
+            status: true,
+          },
+        });
+      } else {
+        const getCourseStudentUnit = await prisma.CourseUnits.findFirst({
+          where: {
+            StudentCourseId: StudentCourseId,
+            unitNumber: req.params.unit,
+          },
+        });
+
+        if (getCourseStudent) {
+          await prisma.CourseUnits.update({
+            where: {
+              id: getCourseStudentUnit.id,
+            },
+            data: {
+              status: true,
+            },
+          });
+        }
+      }
     }
   } catch (error) {
     console.log("error form catch: " + error);
@@ -83,9 +110,9 @@ router.get("/removeone/:studentId/:courseId/:unit", async (req, res, next) => {
     console.log(
       "rec: " +
         req.params.studentId +
-        " " +
+        "  " +
         req.params.courseId +
-        " " +
+        "  " +
         req.params.unit
     );
     // const CitySelected = await prisma.CourseUnits.findUnique({
@@ -100,10 +127,74 @@ router.get("/removeone/:studentId/:courseId/:unit", async (req, res, next) => {
         studentsId: req.params.studentId,
         coursesId: req.params.courseId,
       },
+      include: { units: true },
     });
 
     if (getCourseStudent) {
       const StudentCourseId = getCourseStudent.id;
+      console.log("StudentCOurseId: " + StudentCourseId);
+
+      console.log("Idd: " + getCourseStudent.units[req.params.unit - 1].id);
+      console.log("Full data: " + JSON.stringify(getCourseStudent));
+
+      const getCourseStudentUnit = await prisma.CourseUnits.findFirst({
+        where: {
+          StudentCourseId: StudentCourseId,
+          unitNumber: req.params.unit,
+        },
+      });
+
+      console.log("GetCOurseStudentUnit: " + getCourseStudentUnit);
+      console.log(
+        "GetCOurseStudentUnit: " + JSON.stringify(getCourseStudentUnit)
+      );
+      console.log("Course Unit Id: " + getCourseStudentUnit.id);
+
+      await prisma.CourseUnits.update({
+        where: {
+          id: getCourseStudentUnit.id,
+        },
+        data: {
+          status: false,
+        },
+      });
+    }
+  } catch (error) {
+    console.log("error form catch: " + error);
+    next(error);
+  }
+});
+
+router.get("/removeon/:studentId/:courseId/:unit", async (req, res, next) => {
+  try {
+    //const { id } = req.params.id;
+    console.log(
+      "rec: " +
+        req.params.studentId +
+        "  " +
+        req.params.courseId +
+        "  " +
+        req.params.unit
+    );
+    // const CitySelected = await prisma.CourseUnits.findUnique({
+    //   where: {
+    //     id: req.params.id,
+    //   },
+    // });
+    //res.json(CitySelected);
+
+    const getCourseStudent = await prisma.StudentCourse.findFirst({
+      where: {
+        studentsId: req.params.studentId,
+        coursesId: req.params.courseId,
+      },
+      include: { units: true },
+    });
+
+    if (getCourseStudent) {
+      const StudentCourseId = getCourseStudent.id;
+      console.log("Idd: " + getCourseStudent.units[1].id);
+      console.log("Full data: " + JSON.stringify(getCourseStudent));
 
       const getCourseStudentUnit = await prisma.CourseUnits.findFirst({
         where: {
