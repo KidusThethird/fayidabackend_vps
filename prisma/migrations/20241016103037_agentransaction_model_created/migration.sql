22ec517c89e52f4398a43ecc6cf75d4bfca87cf0
= <kidus.z3rd@gmail.com>
2024-10-16 10:31:34 +0000

    update from vps: model added agent transaction

---
+++ prisma/migrations/20241016103037_agentransaction_model_created/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `agentTransaction` (
+    `id` VARCHAR(191) NOT NULL,
+    `promocode` VARCHAR(191) NOT NULL,
+    `value` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NULL DEFAULT 'paid',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
