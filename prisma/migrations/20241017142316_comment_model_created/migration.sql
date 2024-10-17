afbc8e2ea8b4474437b75663227f4d130eda4347
= <kidus.z3rd@gmail.com>
2024-10-17 14:24:00 +0000

    update form vps: comment model added to schema

---
+++ prisma/migrations/20241017142316_comment_model_created/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `Comments` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `commentFrom` VARCHAR(191) NULL,
+    `text` TEXT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
