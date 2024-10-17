1a9d5cce8b3ec9b6a55d1a10e46eb0c96a227cce
kidus Tesfaye <kidus.z3rd@gmail.com>
2024-10-17 09:05:01 +0300

    update: schema updated

--- configFIles.js
+++ configFIles.js
@@ -1,8 +1,8 @@
 const homeWebUrl = "https://fayidaacademy.com";
 
 const adminUrl = "https://admin.fayidaacademy.com";
-const localUrl = "https://api.fayidaacademy.com";
-//const localUrl = "http://localhost:5000";
+//const localUrl = "https://api.fayidaacademy.com";
+const localUrl = "http://localhost:5000";
 //const localUrl = "http://192.168.1.5:5000";
 
 const apiPort = 5000;
