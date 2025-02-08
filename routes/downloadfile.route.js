e3baa7d3cd0d3d473db56dda30c0d5a8b88da6e8
Kidus <kidus.z3rd@gmail.com>
2025-02-08 07:59:14 +0300

    update: mobile app updated

--- routes/downloadfile.route.js
+++ routes/downloadfile.route.js
@@ -5,20 +5,18 @@ const path = require("path");
 const prisma = new PrismaClient();
 const checkAuthenticated = require("./login_register.route");
 
-
-
 router.use(express.static(path.join(__dirname, "public")));
 
 // Optional: Create a custom endpoint for downloading the APK
 router.get("/", (req, res) => {
-  const filePath = path.join(__dirname, "../apk/fayidaapp.apk");
-  res.download(filePath, "Fayida Academy.apk", (err) => {
+  // const filePath = path.join(__dirname, "../apk/fayidaapp.apk");
+  const filePath = path.join(__dirname, "../apk/fayidamobileapp.apk");
+  res.download(filePath, "Fayida Academy App.apk", (err) => {
     if (err) {
       console.error("Error downloading the file:", err);
       res.status(500).send("Error downloading the file.");
     }
   });
 });
-
 
 module.exports = router;
